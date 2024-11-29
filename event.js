25a6c9f06470ae0cfb5db7b5ad6c532c7aec790b
Avinash Kant <contact.avinashkantse@gmail.com>
2024-11-29 13:12:55 +0530

    Update event.js

--- event.js
+++ event.js
@@ -6,7 +6,6 @@ var eventEmitter = new events.EventEmitter();
 
 // Create an event handler as follows
 var connectHandler = function connected() {
-   console.log('connection succesful.');
   
    // Fire the data_received event 
    eventEmitter.emit('data_received');
@@ -17,7 +16,7 @@ eventEmitter.on('connection', connectHandler);
  
 // Bind the data_received event with the anonymous function
 eventEmitter.on('data_received', function() {
-   console.log('data received succesfully.');
+   //console.log('data received succesfully.');
 });
 
 // Fire the connection event 
